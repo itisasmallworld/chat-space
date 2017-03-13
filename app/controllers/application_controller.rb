75712a6942588d04f5bd2aff0b6358d79ae52029
itisasmallworld <itisasmallworld01270@gmail.com>
2017-03-13 12:24:55 +0900

    Edit-user-information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :after_sign_out_pass_for, if: :devise_controller?
 
-
-
-
   def after_sign_out_pass_for
     'new_user_sesson'
   end
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
 end
