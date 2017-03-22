8f34321f1a38b629852ecd96874659f8e940dd55
itisasmallworld <itisasmallworld01270@gmail.com>
2017-03-22 18:58:04 +0900

    Implement-sending-messages-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,23 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: "メッセージが投稿されました"
+    else
+      flash[:alert] = "メッセージを入力してください"
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:image, :body).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
