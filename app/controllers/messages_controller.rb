2f3113fe1e6b37f3bc8397ee09a47dc41840683b
itisasmallworld <itisasmallworld01270@gmail.com>
2017-03-24 18:29:59 +0900

    Modify-messages-controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
 
-  before_action :set_group, :set_message, only:[:index, :create]
+  before_action :set_group, :set_groups, :set_message, only:[:index, :create]
 
   def index
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path, notice: "メッセージが投稿されました"
     else
@@ -21,12 +23,14 @@ class MessagesController < ApplicationController
   end
 
   def set_group
-    @groups = current_user.groups
     @group = Group.find(params[:group_id])
   end
 
+  def set_groups
+    @groups = current_user.groups
+  end
+
   def set_message
-    @message = Message.new
     @messages = @group.messages
   end
 
