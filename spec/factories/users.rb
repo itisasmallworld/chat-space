69f1434a1dfb345a7a8275c8463f22fd79499488
itisasmallworld <itisasmallworld01270@gmail.com>
2017-03-28 20:47:38 +0900

    Test-posting-function-controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :user do
     email                 {Faker::Internet.email}
-    password              "{1#{Faker::Lorem.characters(8)}}"
+    password              "{1#{Faker::Internet.password(8)}}"
     name                  {Faker::Name.name}
     created_at            {Faker::Time.between(2.days.ago, Time.now)}
   end
